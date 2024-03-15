076cadbb2158759ac34aadeaee3f6de2d3396d09
Cidscapital <jesse.jason2002@gmail.com>
2024-03-16 02:46:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "./vpc"
   region             = "us-east-1"
   vpc_cidr_block     = "192.168.0.0/16"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  availability_zones = "us-east-1"
 }
 
 module "security_group" {
