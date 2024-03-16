3bec7738aeabbc552ae3b5adbed1274baa764155
Cidscapital <jesse.jason2002@gmail.com>
2024-03-16 13:16:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "tf-vpc" {
   cidr_block     = "192.168.0.0/16"
   enable_dns_support = "true" #gives you an internal domain name
   enable_dns_hostnames = "true" #gives you an internal host name
-  enable_classiclink = "false"
   instance_tenancy = "default"    
   tags = {
     Name = "tf-vpc"
